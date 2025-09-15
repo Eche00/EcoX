50396d47025601fbdb7559473772f149b5a1f1d1
Eche00 <echeeze956@gmail.com>
2025-09-15 01:16:04 +0100

    page size and bg color edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import Container from "./component/Container";
 import Home from "./pages/Home";
-import ThemeModal from "./component/theme/ThemeModal";
 
 function App() {
   return (
-    <div className="   overflow-hidden bg-[#F0F8FF]  dark:bg-[#0c0d13]">
+    <div className="w-full overflow-hidden bg-[#F0F8FF]  dark:bg-[#0c0d13]">
       <Routes>
         <Route element={<Container />}>
-          <Route path="/" element={<ThemeModal />} />
+          <Route path="/" element={<Home />} />
         </Route>
       </Routes>
     </div>
