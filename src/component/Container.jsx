9c532466d726300de77c70c31e749700eaa1608a
Eche00 <echeeze956@gmail.com>
2025-09-15 01:16:28 +0100

    container size and bg color fixtures

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -4,12 +4,12 @@ import Header from "./header/Header";
 
 function Container() {
   return (
-    <div className="h-[100vh] relative sm:h-full bg-[#F0F8FF] dark:bg-[#0c0d13]">
+    <div className=" relative bg-[#F0F8FF] dark:bg-[#0c0d13] overflow-hidden">
       {/* header  */}
       <Header />
 
       {/* outlet for pages   */}
-      <div className="ml-0 md:ml-[300px] flex-1 p-5 h-[100vh]">
+      <div className=" flex-1 min-h-[100vh]">
         <Outlet />
       </div>
     </div>
